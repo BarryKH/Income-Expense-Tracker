6348a59d48a90fbf612ce0455cd93eebd2d21694
BarryKH <barryhu1209@gmail.com>
2025-09-12 20:12:47 -0400

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 
-st.title("Income & Expend")
+st.title("Income & Expense")
 st.subheader("Data")
 #data
 totalMonths = st.number_input("Total Months", min_value=1, max_value=12, value=3)
@@ -12,9 +12,9 @@ for eachMonth in range(totalMonths):
     st.write(f"### Month {currentMonth}")
     month = st.text_input(f"Month {currentMonth+1} (e.g., Jan)", key=f"m{currentMonth}")
     income = st.number_input(f"Income for {month}", key = f"inc{currentMonth-1}", step = 100.0)
-    expend = st.number_input(f"Expend for {month}", key = f"exp{currentMonth-1}", step = 100.0)
+    expense = st.number_input(f"Expense for {month}", key = f"exp{currentMonth-1}", step = 100.0)
     if month:
-        data.append({"Month":month, "Income":income, "Expend":expend, "Savings":income - expend})
+        data.append({"Month":month, "Income":income, "Expense":expense, "Savings":income - expend})
     currentMonth += 1
 if data:
     df = pd.DataFrame(data)
@@ -23,8 +23,8 @@ if data:
 
     df["Total Savings"] = df["Savings"].cumsum()
 #income&expend
-    fig = px.line(df, x="Month", y=["Income","Expend"], markers=True,
-                  title="Income vs. Expend")
+    fig = px.line(df, x="Month", y=["Income","Expense"], markers=True,
+                  title="Income vs. Expense")
     st.plotly_chart(fig)
 #total savings
     fig2 = px.line(df, x="Month", y=["Total Savings"], markers=True,
