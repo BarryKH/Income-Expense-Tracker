b40f0a3ff3f09487fe08d302a0d41c4c0dd40e64
BarryKH <barryhu1209@gmail.com>
2025-09-12 20:46:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ currentMonth = 0
 for eachMonth in range(totalMonths):
     st.write(f"### Month {currentMonth}")
     month = st.text_input(f"Month {currentMonth+1} (e.g., Jan)", key=f"m{currentMonth}")
-    income = st.number_input(f"Income for {month}", key = f"inc{currentMonth-1}", step = 100.0)
-    expense = st.number_input(f"Expense for {month}", key = f"exp{currentMonth-1}", step = 100.0)
+    income = st.number_input(f"Income for {month}", key = f"inc{currentMonth}", step = 100.0)
+    expense = st.number_input(f"Expense for {month}", key = f"exp{currentMonth}", step = 100.0)
     if month:
         data.append({"Month":month, "Income":income, "Expense":expense, "Savings":income - expense})
     currentMonth += 1
